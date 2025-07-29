d9132684c9329009cf32702f0eb267047c8c0405
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-07-29 15:25:19 +0200

    [UPD] Add Typography

--- storybook/src/stories/Spacing.stories.tsx
+++ storybook/src/stories/Spacing.stories.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const meta: Meta = {
     title: "Atoms/Spacing",
     parameters: {
-        layout: "centered",
+        layout: 'centered'
     },
     tags: ["autodocs"],
 };
@@ -13,54 +13,79 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-const spacingKeys = ["zero", "xs", "sm", "md", "lg", "xl"];
-
-const SpaceCalc = ({ value }: { value: string }) => {
+const SpaceCalculation = ({ value }: { value: string }) => {
     const spacing = React.useMemo(() => {
-        const rootStyle = getComputedStyle(document.documentElement);
-        return rootStyle.getPropertyValue(value);
+        const bodyStyle = window.getComputedStyle(document.body);
+        return bodyStyle.getPropertyValue(value);
     }, [value]);
     return <span>{spacing}</span>;
 };
 
-const Style: React.FC = () => (
-    <style>{`
-        dl {
-            font-size: 1rem;
-            border: 1px solid #ccc;
-            display: grid;
-            max-width: calc(550rem / 16);
-            grid-template-columns: 1fr 1fr;
-            border-bottom: none;
-            border-radius: 0.5rem;
-    }
-        dt {
-            font-weight: 600;
-            padding: var(--spacing-sm) var(--spacing-md);
-            border-bottom: 1px solid #ccc;
-        }
-        dd {
-            padding: var(--spacing-sm) var(--spacing-md);
-            border-bottom: 1px solid #ccc;
-            font-family: monospace;
-        }
-    `}</style>
-);
+const Style: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
+    return <>
+        <style>{`
+            dl {
+                font-size: 1rem;
+                border: 1px solid #ccc;
+                max-width: calc(550rem / 16);
+                display: grid;
+                grid-template-columns: 1fr 1fr 1fr;
+                border-bottom: none;
+                border-radius: 5px;
+            }
+            dt {
+                font-weight: 600;
+                padding: var(--spacing-sm)  var(--spacing-md);
+                border-bottom: 1px solid #ccc;
+            }
+            dd {
+                border-bottom: 1px solid #ccc;
+                padding: var(--spacing-sm)  var(--spacing-md);
+                font-family: monospace;
+                display: grid;
+                grid-template-columns: subgrid;
+                gap: 1ch;
+                grid-column: 2/4;
+                align-items: center;
+            }
+            .info {
+                background-color:rgba(4, 255, 4, 1);
+                height: var(--story-spacing);
+                border-radius: 2px;
+                width: var(--story-spacing);
+                display: block;
+         }
+        `}</style>
+        {children}
+    </>;
+}
+
+const dimensions = ["zero", "xs", "sm", "md", "lg", "xl"] as const;
 
 export const Default: Story = {
-    render: () => (
-        <>
-            <Style />
+    render: () => <>
+
+        <h1>Spacing</h1>
+        <p style={{ maxWidth: "55ch" }}>
+            Our spacing variables use a clamping mechanism to ensure that the
+            spacing is scalable across different screen sizes. * This is done by
+            using the `clamp()` function. * The minimum viewport width is 400px and
+            the maximum is 1200px.
+        </p>
+        <Style>
             <dl>
-                {spacingKeys.map((key) => (
+                {dimensions.map((key) => (
                     <React.Fragment key={key}>
                         <dt>{key}</dt>
-                        <dd>
-                            <SpaceCalc value={`--spacing-${key}`} />
+                        <dd style={{ '--story-spacing': `var(--spacing-${key})` } as React.CSSProperties}>
+                            <span>
+                                <SpaceCalculation value={`--spacing-${key}`} />
+                            </span>
+                            <span className="info" />
                         </dd>
                     </React.Fragment>
                 ))}
             </dl>
-        </>
-    ),
-};
+        </Style>
+    </>,
+}
